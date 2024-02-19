23918979c59117b0ebd9b694a2ab32d94d12dfff
Tarnest <tristanconnolly1@gmail.com>
2024-02-19 17:16:15 -0500

    Added Confirm Button

---
+++ Main/cauldron.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func add_to_pot(slot: InventorySlot):
+	pass
--- crafting_objects.gd
+++ crafting_objects.gd
@@ -4,6 +4,7 @@ signal open_menu
 
 @onready var player_inventory = preload("res://Player/player_inventory.tres")
 var object_closed = true
+var items_in_pot: Array[InventoryItem]
 
 
 func open():
@@ -19,3 +20,8 @@ func use_item(item_name):
 	var item = player_inventory.get_item(item_name)
 	if item != null:
 		player_inventory.remove(item)
+		add_to_pot(item)
+
+
+func add_to_pot(item: InventoryItem):
+	items_in_pot.append(item)
