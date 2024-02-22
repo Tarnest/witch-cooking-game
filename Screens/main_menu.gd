58ef73836499fbb32193adbeb90089850aaf84d2
unknown <Jake.busekrus@gmail.com>
2024-02-22 14:03:39 -0500

    Set up the basic main menu

---
+++ Screens/main_menu.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if Input.is_action_pressed("escape_game"):
+			get_tree().quit()
--- Screens/win_screen.gd
+++ Screens/win_screen.gd
@@ -9,3 +9,11 @@ func _on_exit_timer_timeout():
 	else:
 		exit_text.show()
 	exit_timer.start()
+
+func _input(event):
+	if event.is_action_pressed("back_to_main"):
+		get_tree().change_scene_to_file("res://Screens/main_menu.tscn")
+		
+func _process(_delta):
+	if Input.is_action_pressed("escape_game"):
+		get_tree().quit()
