db7c19ee34cbf1d30a1351e91dd323b42ba7614e
Tarnest <tristanconnolly1@gmail.com>
2024-02-25 00:05:55 -0500

    Added Items, Trash Can, Shelves, Bed

--- Inventory/inventory.gd
+++ Inventory/inventory.gd
@@ -6,9 +6,29 @@ signal update
 @export var slots: Array[InventorySlot]
 @export var max_items: int = 3
 
-var eyeball = preload("res://Objects/InventoryItems/Eyeball/eyeball.tres")
-var ruby = preload("res://Objects/InventoryItems/Ruby/ruby.tres")
-var red_potion = preload("res://Objects/InventoryItems/RedPotion/red_potion.tres")
+var aloe = preload("res://Objects/InventoryItems/FarmItems/Aloe/aloe.tres")
+var apple = preload("res://Objects/InventoryItems/FarmItems/Apple/apple.tres")
+var rose = preload("res://Objects/InventoryItems/FarmItems/Rose/rose.tres")
+var sage = preload("res://Objects/InventoryItems/FarmItems/Sage/sage.tres")
+var tulip = preload("res://Objects/InventoryItems/FarmItems/Tulip/tulip.tres")
+
+var bottle = preload("res://Objects/InventoryItems/ShelfItems/Bottle/bottle.tres")
+var chicken_foot = preload("res://Objects/InventoryItems/ShelfItems/ChickenFoot/chicken_foot.tres")
+var emerald = preload("res://Objects/InventoryItems/ShelfItems/Emerald/emerald.tres")
+var eyeball = preload("res://Objects/InventoryItems/ShelfItems/Eyeball/eyeball.tres")
+var feather = preload("res://Objects/InventoryItems/ShelfItems/Feather/feather.tres")
+var gold = preload("res://Objects/InventoryItems/ShelfItems/Gold/gold.tres")
+var horn = preload("res://Objects/InventoryItems/ShelfItems/Horn/horn.tres")
+var limestone = preload("res://Objects/InventoryItems/ShelfItems/Limestone/limestone.tres")
+var lizard_tail = preload("res://Objects/InventoryItems/ShelfItems/LizardTail/lizard_tail.tres")
+var paper = preload("res://Objects/InventoryItems/ShelfItems/Paper/paper.tres")
+var rope = preload("res://Objects/InventoryItems/ShelfItems/Rope/rope.tres")
+var ruby = preload("res://Objects/InventoryItems/ShelfItems/Ruby/ruby.tres")
+var tongue = preload("res://Objects/InventoryItems/ShelfItems/Tongue/tongue.tres")
+var twig = preload("res://Objects/InventoryItems/ShelfItems/Twig/twig.tres")
+var wax = preload("res://Objects/InventoryItems/ShelfItems/Wax/wax.tres")
+
+var red_potion = preload("res://Objects/InventoryItems/CauldronProducts/RedPotion/red_potion.tres")
 
 var possible_items: Array[InventoryItem] = [eyeball, ruby, red_potion]
 
